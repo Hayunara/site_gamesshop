70aecaaab6d32f3cb5cd9b1304b46abf57a07de3
Hayunara <hayunara@outlook.com>
2025-04-15 11:20:24 -0300

    ajustando configurações

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $(".btn-menu").click(function() {
+        $(".menu").slideToggle();
+    });
+
+    $("#telefone").mask("(00) 00000-0000");
+
+    $("form").validate ({
+        rules:{
+            nome:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            mensagem:{
+                required: true
+            }
+        },
+        messages:{
+            nome: `<span > Por favor, insira seu nome </span> `,
+            email: `<span > E-mail invalido </span> `,
+            menssagem: `<span > Por favor, insira sua mensagem </span> `,
+        },
+    })
+})
